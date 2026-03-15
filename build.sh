0a2792936b7271e178445eb762b1b253a843844c
Pratik-poudel1 <pratikpoudel676@gmail.com>
2026-03-15 16:59:07 +0545

    Configure for Render deployment

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Create superuser if none exists
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('admin', 'admin@taskmanager.com', 'admin@123')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+"
